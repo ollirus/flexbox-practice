d16edf962acb5101125439b391122b19a9abc45d
Aleksey Oliferchik <a.oliferchik@wsmgroup.ru>
2017-11-02 22:56:54 +0200

    example #1 (Layout with Flexbox)

--- Flexbox/App.js
+++ Flexbox/App.js
@@ -1,13 +1,52 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
 
-export default class App extends React.Component {
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' +
+    'Cmd+D or shake for dev menu',
+  android: 'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
+export default class App extends Component<{}> {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <View style={[styles.box, {
+          backgroundColor: 'powderblue',
+          // flexGrow: 1, 
+          // flexShrink: 1,          
+          }]} />
+        <View style={[styles.box, {
+          backgroundColor: 'skyblue',
+          maxHeight: 100,
+          // flexGrow: 4,
+          // flexShrink: 2,
+          }]} />
+        {/* <View style={[styles.box, {backgroundColor: 'steelblue'}]} />
+
+        <View style={[styles.box, {backgroundColor: 'powderblue'}]} />
+        <View style={[styles.box, {backgroundColor: 'skyblue'}]} />
+        <View style={[styles.box, {backgroundColor: 'steelblue'}]} />
+
+        <View style={[styles.box, {backgroundColor: 'powderblue'}]} />
+        <View style={[styles.box, {backgroundColor: 'skyblue'}]} />
+        <View style={[styles.box, {backgroundColor: 'steelblue'}]} />
+
+        <View style={[styles.box, {backgroundColor: 'powderblue'}]} />
+        <View style={[styles.box, {backgroundColor: 'skyblue'}]} />
+        <View style={[styles.box, {backgroundColor: 'steelblue'}]} /> */}
       </View>
     );
   }
@@ -16,8 +55,24 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // determines the primary axis of its layout: 
+    // row or column
+    flexDirection: 'row',
+    // determines the distribution of children along the primary axis
+    // flex-start, center, flex-end, space-around, and space-between
+    justifyContent: 'flex-start',
+    // determines the alignment of children along the secondary axis
+    // flex-start, center, flex-end, and stretch.
+    alignItems: 'stretch',
+    // flexWrap: 'wrap',    
+    // top: 30,
+    backgroundColor: 'red',
+    borderWidth: 50,
+  },
+  box: {
+    width: 50, 
+    // height: 50,
+    // marginHorizontal: 5,
+    // marginVertical: 5,
   },
 });
